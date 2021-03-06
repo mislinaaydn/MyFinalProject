da4ee0cddb6a9f5ac651fcc9ef128c53feec9d0c
mislinaaydn <mislinaaydn26305@gmail.com>
2021-03-06 17:19:21 +0300

    kurs doküman

---
+++ Core/Aspects/Autofac/Performance/PerformanceAspect.cs
@@ -0,0 +1,40 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IOC;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Core.Aspects.Autofac.Performance
+{
+    public class PerformanceAspect : MethodInterception
+    {
+        private int _interval;
+        private Stopwatch _stopwach;
+
+
+        public PerformanceAspect(int interval)
+        {
+            _interval = interval;
+            _stopwach = ServiceTool.ServiceProvider.GetService<Stopwatch>();
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _stopwach.Start();
+        }
+
+        protected override void OnAfter(IInvocation invocation)
+        {
+            if (_stopwach.Elapsed.TotalSeconds > _interval)
+            {
+                Debug.WriteLine($"Performance : {invocation.Method.DeclaringType.FullName}.{invocation.Method.Name}-->{_stopwach.Elapsed.TotalSeconds}");
+            }
+            _stopwach.Reset();
+        }
+
+    }
+}
